2a5d284aac087657de2d899299f585daadb75c21
rdam1 <31661538+rdam1@users.noreply.github.com>
2017-09-07 11:51:08 -0400

    Create Project: What's for Dinner?.js

---
+++ Project: What's for Dinner?.js
@@ -0,0 +1,23 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300);
+fill(217, 195, 154); //dough
+stroke(181, 151, 67); //crust
+strokeWeight(20);
+arc(275,275,400,400,200,250); //pizza shape thingy
+arc(275,275,385,385,200,250);
+fill(255, 225, 125); //cheese
+stroke(161, 107, 53);
+strokeWeight(1);
+arc(275,275,365,365,200,250);
+fill(156, 0, 0); //pepperoni
+stroke(102, 0, 0);
+ellipse(200,150,30,30);
+ellipse(150,175,30,30);
+ellipse(175,240,30,30);
+ellipse(195,200,30,30);
+ellipse(125,225,30,30);
+ellipse(235,235,30,30);
+stroke(255, 255, 255); //fill
+strokeWeight(20);
+line(200,260,110,229);
