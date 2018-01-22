fd441382a44fef912344c351b975eea0dcc6b793
Ryan Dam <31661538+rdam1@users.noreply.github.com>
2018-01-22 10:00:29 -0500

    Create favourite fruits.js

---
+++ intro to JS/challenges/arrays/favourite fruits.js
@@ -0,0 +1,7 @@
+var fruits = ["apple", "pear", "orange"];
+
+fill(255, 0, 0);
+text("My top " + fruits.length + " favourite fruits", 1, 10);
+text(fruits[0], 1,20);
+text(fruits[1], 1,30);
+text(fruits[2], 1,40);
