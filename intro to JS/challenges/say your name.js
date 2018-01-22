a9ce0f1d484c9fec8f77bbfab2d2de9e9f28fa59
Ryan Dam <31661538+rdam1@users.noreply.github.com>
2018-01-22 09:45:52 -0500

    Create say your name.js

---
+++ intro to JS/challenges/say your name.js
@@ -0,0 +1,12 @@
+var yourName = "Ryan";
+var name = function() {
+var textX = random(0, 300);
+var textY = random(0, 300);    
+fill(255, 0, 0);
+textSize(30);
+text("Hiiii, " + yourName, textX, textY);
+};
+name();
+name();
+name();
+name();
